565ba43882fce4c273a486125f58666b437d2ee4
Tyr.Phoon <102190282@sv1.dut.udn.vn>
2021-12-27 21:22:52 +0700

    Update Exercise 4

--- Bai4.py
+++ Bai4.py
@@ -2,10 +2,16 @@
 
 class Solution:
     def distributeCandies(self, a: List[int]) -> int:
-        b =[]
-        for i in range(len(a)):
-            if(a[i] in b) == False:
-                b.append(a[i])
-            if(len(b)==len(a)/2): break
-        kq = int(min(len(a)/2, len(b)))
-        return kq
+        if (len(set(a)) > len(a)/2): return int(len(a)/2)
+        else: return int(len(set(a)))
+
+
+# class Solution:
+#     def distributeCandies(self, a: List[int]) -> int:
+#         b =[]
+#         for i in range(len(a)):
+#             if(a[i] in b) == False:
+#                 b.append(a[i])
+#             if(len(b)==len(a)/2): break
+#         kq = int(min(len(a)/2, len(b)))
+#         return kq
